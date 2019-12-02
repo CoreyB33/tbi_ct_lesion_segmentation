83dd7ce51c84e149b9d045a9bbf2b1f895643568
CoreyB33 <44987324+CoreyB33@users.noreply.github.com>
2019-12-02 12:57:29 -0600

    Update apply_model.py

--- utils/apply_model.py
+++ utils/apply_model.py
@@ -32,7 +32,8 @@ def apply_model(img_volume, model):
         # the [0] index at the start refers to the first of two outputs,
         # since this is a dual-output network
         # the [1] index is the auxiliary output
-        out_vol[:, :, k] = pred[0][0, :, :, 0]
+        ## Adding in an extra 0
+        out_vol[:, :, k] = pred[0][0, :, :, 0, 0]
 
     return out_vol
 
