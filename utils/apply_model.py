0bc0d2e298972fc85f73ae6c139e1da216c1b3e8
CoreyB33 <44987324+CoreyB33@users.noreply.github.com>
2019-12-02 13:03:35 -0600

    Update apply_model.py

--- utils/apply_model.py
+++ utils/apply_model.py
@@ -28,7 +28,7 @@ def apply_model(img_volume, model):
             img_slice[0, :, :, c] = img_volume[:, :, k, c]
 
         pred = model.predict([img_slice, aux_slice])
-        print("Predictor = {}".format(pred))
+        print("Predictor = {}".format(pred.shape))
         print("img slice size = {}".format(img_slice.shape))
         # the [0] index at the start refers to the first of two outputs,
         # since this is a dual-output network
