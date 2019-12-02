0bfdd634724c6f4075e250c1f4b98fceef91b14a
CoreyB33 <44987324+CoreyB33@users.noreply.github.com>
2019-12-02 15:37:21 -0600

    Update apply_model.py

--- utils/apply_model.py
+++ utils/apply_model.py
@@ -31,13 +31,10 @@ def apply_model(img_volume, model):
     for k in tqdm(range(num_slices)):
         for c in range(num_channels):
             img_slice[0, :, :, c] = img_volume[:, :, k, c]
-            print("img slice shape = {}".format(img_slice.shape))
+
             
         #May need to change to ([img_slice[0,:,:,0], img_slice[0,:,:,1])
-        pred = model.predict([img_slice, aux_slice])
-        print("img slice shape 2 = {}".format(img_slice.shape))
-        print("aux slice shape = {}".format(aux_slice.shape))
-        print("aux unique values = {}".format(np.unique(aux_slice)))
+        pred = model.predict([img_slice[0,:,:,0], img_slice[0,:,:,1]])
          
         # the [0] index at the start refers to the first of two outputs,
         # since this is a dual-output network
