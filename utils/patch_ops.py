dee9f900952288d17ec17cf9a259d45ae59362b0
CoreyB33 <44987324+CoreyB33@users.noreply.github.com>
2020-01-30 13:14:01 -0600

    Update patch_ops.py

--- utils/patch_ops.py
+++ utils/patch_ops.py
@@ -8,8 +8,8 @@ import random
 import copy
 from time import strftime, time
 
-import rotation
-from rotation import rotateit
+#import rotation
+#from rotation import rotateit
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
