84ce61cadbae398583b0d2194b195d53542df4bc
CoreyB33 <44987324+CoreyB33@users.noreply.github.com>
2020-01-30 13:54:32 -0600

    Update train.py

--- train.py
+++ train.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
        #                       SKULLSTRIP_SCRIPT_PATH,)
 
     ######### DATA IMPORT #########
-    t1_patches, flair_patches, mask_patches = patch_ops.CreatePatchesForTraining(
+    t1_patches, mask_patches = patch_ops.CreatePatchesForTraining(
         atlasdir=DATA_DIR,
         plane=plane,
         patchsize=PATCH_SIZE,
@@ -131,8 +131,7 @@ if __name__ == "__main__":
         t1_patches.shape, mask_patches.shape))
 
     ######### TRAINING #########
-    history = model.fit([t1_patches,
-                        flair_patches],
+    history = model.fit(t1_patches,
                         mask_patches,
                         batch_size=batch_size,
                         epochs=num_epochs,
