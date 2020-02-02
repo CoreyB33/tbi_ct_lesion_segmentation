ad5022630ddf556d166eaff16a33b146ca62413a
CoreyB33 <44987324+CoreyB33@users.noreply.github.com>
2020-02-02 16:25:38 -0600

    Update train.py

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ import numpy as np
 import os
 from subprocess import Popen, PIPE
 import sys
-#import rotation
-#from rotation import rotateit
+import rotation
+from rotation import rotateit
 
 from utils import utils, patch_ops
 from utils import preprocess
