0aa570ad6611e03e467ab1843847f38af76ac1a0
CoreyB33 <44987324+CoreyB33@users.noreply.github.com>
2020-02-04 10:16:49 -0600

    Update patch_ops.py

--- utils/patch_ops.py
+++ utils/patch_ops.py
@@ -173,7 +173,8 @@ def get_patches(invols, mask, patchsize, maxpatch, num_channels):
 
     t1Patches = np.ndarray(t1_matsize, dtype=np.float16)
     MaskPatches = np.ndarray(Mask_matsize, dtype=np.float16)
-
+    
+    sess=tf.InteractiveSession()
     for i in range(0, 2*num_patches):
         I = newidx[0, i]
         J = newidx[1, i]
