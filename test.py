2e1163a9688a93cef9361fbbd2414396a6551388
CoreyB33 <44987324+CoreyB33@users.noreply.github.com>
2020-02-06 15:43:10 -0600

    Update test.py

--- test.py
+++ test.py
@@ -94,9 +94,9 @@ if __name__ == "__main__":
     ######################## SEGMENT FILES ########################
     filenames = [x for x in os.listdir(DATA_DIR)
                  if not os.path.isdir(os.path.join(DATA_DIR, x))]
-    masks = [x for x in filenames if "PVS" in x]
+    masks = [x for x in filenames if "pvs" in x]
     # Using 4D file instead of just t1, if using just t1, use "t1" instead of "multi"
-    filenames = [x for x in filenames if "t1_masked_rescale" in x]
+    filenames = [x for x in filenames if "t1_masked" in x]
     
 
     filenames.sort()
